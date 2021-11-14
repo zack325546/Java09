434a24f64641a0f7bfff7a13833bdd19bcc5bbf3
zack325546 <91444369+zack325546@users.noreply.github.com>
2021-11-14 23:45:44 +0800

    Create Class03.java

---
+++ Class03.java
@@ -0,0 +1,18 @@
+class Caaa{//(c)否,因為會與文件名衝突
+	private int value;//(d)可以,不會有影響,因為其他的建構元會再對value做設定
+	
+	public Caaa(){
+		value=10;//(a)
+		System.out.println("value="+value);
+	}
+	public Caaa(int i){
+		value=i;
+		System.out.println("value="+value);
+	}
+}
+public class TESTB{
+	public static void main(String args[]) {
+		Caaa obj1=new Caaa();//(b)public Caaa()
+		Caaa obj2=new Caaa(12);//(b)public Caaa(int i)
+	}
+}
