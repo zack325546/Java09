138987ff70020c4651ee711d620b8f955220f6c9
zack325546 <91444369+zack325546@users.noreply.github.com>
2021-11-14 23:04:13 +0800

    Create Class01.java

---
+++ Class01.java
@@ -0,0 +1,15 @@
+class Crectangle{
+	int width;
+	int height;
+
+public Crectangle(int w,int h){      
+	width=w;
+	height=h;
+}
+
+public Crectangle(){      
+	this(10,8);
+	//width=10;
+	//height=8;
+}
+}
